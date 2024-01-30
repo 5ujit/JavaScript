05351b52fb67cc8ea61c4fbce0e95856f5d9d309
5ujit <149666757+5ujit@users.noreply.github.com>
2024-01-30 23:45:23 +0530

    first commit

---
+++ 02js_variable.js
@@ -0,0 +1,14 @@
+// // intro to varoables
+
+// variables can store information 
+// we can use that information later 
+// we can change thet information later ]
+
+// declare a variable 
+
+var firstname ="Sujit ";
+console.log(first) 
+// use a variable
+
+// change value
+
